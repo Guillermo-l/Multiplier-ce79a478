6c91c1f70476318295ede8dadc4e77058726bfa0
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-09-16 09:04:03 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Ik ga twee getallen met elkaar vermenigvuldigen. \n";
+
+echo "Eerste getal? \n";
+$eerste_getal = readline();
+
+echo "Tweede getal? \n";
+$tweede_getal = readline();
+
+$uitkomst = $eerste_getal + $tweede_getal;
+
+print("Uw resultaat is: " . $uitkomst);
+
+?>
